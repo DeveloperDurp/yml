78e352138001b2947b7d5d0ca44f84d7462f481a
DeveloperDurp <developerdurp@durp.info>
2023-04-09 20:18:08 +0000

    Update 2 files
    - /templates/build-container.tpl.yml
    - /scripts/version-number.sh

--- scripts/version-number.sh
+++ scripts/version-number.sh
@@ -5,5 +5,5 @@ fi
 export COMMIT_COUNT=$(git rev-list --count HEAD)
 export BUILD_NUMBER=$CI_PIPELINE_ID
 export VERSION="$VERSION.$COMMIT_COUNT.$BUILD_NUMBER"
-echo $VERSION > version.env
+echo "VERSION=$VERSION" > version.env
 cat version.env
