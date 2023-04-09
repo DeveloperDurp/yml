45a7bc18d184d59a6bf054abaa73b18c915268ea
DeveloperDurp <developerdurp@durp.info>
2023-04-09 16:09:48 +0000

    test

--- scripts/install-yq.sh
+++ scripts/install-yq.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #%%MULTILINE_YAML_START
 #Install YQ
-
+#
 export YQ_VERSION=4.27.2
 export YQ_ARCH=linux_amd64 
 curl -fsSL "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_${YQ_ARCH}" -o yq 
