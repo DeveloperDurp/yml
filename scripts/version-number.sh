64f2b387a7149caa113dc414c8c7d5a47ff28bb9
DeveloperDurp <developerdurp@durp.info>
2023-05-16 00:09:11 +0000

    Update 2 files
    - /scripts/version-number.sh
    - /templates/build-container.tpl.yml

--- scripts/version-number.sh
+++ scripts/version-number.sh
@@ -1,9 +1,35 @@
+#!/usr/bin/env bash
+#%%MULTILINE_YAML_START
+#Check for configuration
+echo "mode: ContinuousDeployment
+continuous-delivery-fallback-tag: ''
+branches:
+  master:
+    mode: ContinuousDeployment
+    tag: 'dev'
+    increment: Minor
+    track-merge-target: true
+    tracks-release-branches: true
+    is-release-branch: false
+    prevent-increment-of-merged-branch-version: false
+  release:
+    regex: release?[/]
+    mode: ContinuousDeployment
+    increment: Patch
+    tag: ''
+    is-release-branch: true
+    prevent-increment-of-merged-branch-version: true
+  feature:
+    regex: feature?[/]
+    mode: ContinuousDeployment
+    increment: Inherit
+    tag: 'alpha'
+    is-release-branch: false
+    prevent-increment-of-merged-branch-version: true
+ignore:
+  sha: []
+" > GitVersion.yml
 
-if [ -z "$VERSION" ]; then
-  VERSION="1"
-fi
-export COMMIT_COUNT=$(git rev-list --count HEAD)
-export BUILD_NUMBER=$CI_PIPELINE_ID
-export VERSION="$VERSION.$COMMIT_COUNT.$BUILD_NUMBER"
-echo "VERSION=$VERSION" > version.env
+version=$(/tools/dotnet-gitversion /showvariable NuGetVersionV2)
+echo "VERSION=$version" >> version.env
 cat version.env
