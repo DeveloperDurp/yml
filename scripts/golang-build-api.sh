2870a4a7a5baddaa016a67bd4a8e03b019b901fb
DeveloperDurp <DeveloperDurp@durp.info>
2023-05-21 10:47:06 -0500

    update

--- scripts/golang-build-api.sh
+++ scripts/golang-build-api.sh
@@ -10,4 +10,4 @@ go install
 go install github.com/swaggo/swag/cmd/swag@v1.8.12 
 swag init
 
-go build -o ./output/main .
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./output/main .
