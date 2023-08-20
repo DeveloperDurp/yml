01bf91aeeebea518b6ceb7a5c4cd396b90cff7bf
DeveloperDurp <developerdurp@durp.info>
2023-08-20 13:47:30 +0000

    Update 2 files
    - /scripts/helm-push.sh
    - /templates/helm.tpl.yml

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -2,6 +2,7 @@
 #%%MULTILINE_YAML_START
 #Helm Push    
     
+helm plugin install https://github.com/chartmuseum/helm-push
 for chart in packages/*; do
-    helm push ./$chart oci://$CI_REGISTRY/$CI_PROJECT_PATH
+    helm cm-push ./$chart ${CI_PROJECT_NAME}
 done
