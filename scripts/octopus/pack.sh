d8b2d168be77b5708d5a29cbe8def75726ebd24a
developerdurp <developerdurp@durp.info>
2023-11-21 13:09:12 -0600

    update

--- scripts/octopus/pack.sh
+++ scripts/octopus/pack.sh
@@ -2,9 +2,9 @@
 #%%MULTILINE_YAML_START
 #Package with octopus
 mkdir $CI_PROJECT_DIR/packages
-IFS=";"
-count=0
-for i in $projectname; 
-  do dotnet /octo/octo.dll pack --id=$i --version=$VERSION --outFolder=$CI_PROJECT_DIR/packages --basePath=$WORKDIR/$i;
-  count=$(($count+1));
-  done
+
+for dir in ./output/*/;
+do dir=${dir%#/};
+    dir=${dir##*/};
+    dotnet /octo/octo.dll pack --id=$dir --version=$VERSION --outFolder=$CI_PROJECT_DIR/packages --basePath=$CI_PROJECT_DIR/output/$dir;
+done
