ffef37af6c3f968cafab951d2a2dbe610ac397ba
developerdurp <developerdurp@durp.info>
2023-11-21 13:21:09 -0600

    update

--- scripts/octopus/pack.sh
+++ scripts/octopus/pack.sh
@@ -4,7 +4,7 @@
 mkdir $CI_PROJECT_DIR/packages
 
 for dir in ./output/*/;
-do dir=${dir%#/};
+do dir=${dir%*/};
     dir=${dir##*/};
     dotnet /octo/octo.dll pack --id=$dir --version=$VERSION --outFolder=$CI_PROJECT_DIR/packages --basePath=$CI_PROJECT_DIR/output/$dir;
 done
