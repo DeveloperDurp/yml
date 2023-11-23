5689d6e99a279cd7d0dbe00a42d3789c8238366a
developerdurp <developerdurp@durp.info>
2023-11-23 15:25:57 -0600

    update docker for dev tag

--- scripts/docker/push-gitlab.sh
+++ scripts/docker/push-gitlab.sh
@@ -2,9 +2,15 @@
 #%%MULTILINE_YAML_START
 #Push container to gitlab
 containername=$(echo "$PROJECT_NAME" | awk '{print tolower($0)}')
+if [[ "$VERSION" =~ ^([0-9]+(\.[0-9]+)*)$ ]] 
+then
+  tag="latest" 
+else
+  tag="dev"
+fi
 
 docker import $CI_PROJECT_DIR/packages/$containername.$VERSION.tar.gz $CI_REGISTRY/$CI_PROJECT_PATH:$VERSION
-docker import $CI_PROJECT_DIR/packages/$containername.$VERSION.tar.gz $CI_REGISTRY/$CI_PROJECT_PATH:latest
+docker import $CI_PROJECT_DIR/packages/$containername.$VERSION.tar.gz $CI_REGISTRY/$CI_PROJECT_PATH:$tag
 
 docker push "$CI_REGISTRY/$CI_PROJECT_PATH:$VERSION"
-docker push "$CI_REGISTRY/$CI_PROJECT_PATH:latest"
+docker push "$CI_REGISTRY/$CI_PROJECT_PATH:tag"
