78e551b6862be6c163a5661bfc1d35418e29bc5d
developerdurp <developerdurp@durp.info>
2023-11-25 21:31:51 -0600

    update

--- scripts/gitlab/movefiles.sh
+++ scripts/gitlab/movefiles.sh
@@ -2,8 +2,7 @@
 #%%MULTILINE_YAML_START
 #move files to output
 mkdir $CI_PROJECT_DIR/output
-IFS=";" read -a names <<< $PROJECT_NAME
-for i in $names;
+for i in $(echo $PROJECT_NAME | tr ";" "\n");
   do mkdir $CI_PROJECT_DIR/output/$i;
   cp -r $CI_PROJECT_DIR/$i $CI_PROJECT_DIR/output/;
   done
