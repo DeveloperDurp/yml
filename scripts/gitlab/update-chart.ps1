45075cd136e9aedb102e4e29bcde05c9279cee7d
DeveloperDurp <DeveloperDurp@durp.info>
2024-03-31 09:45:01 -0500

    update

--- scripts/gitlab/update-chart.ps1
+++ scripts/gitlab/update-chart.ps1
@@ -1,6 +1,6 @@
 Install-Module -Name powershell-yaml -Confirm:$false -Force
 
-$template = (Invoke-RestMethod -Headers @{ 'PRIVATE-TOKEN'= $ENV:CI_JOB_TOKEN } -Uri "https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml/raw?ref=main") | ConvertFrom-Yaml
+$template = (Invoke-RestMethod -Headers @{ 'PRIVATE-TOKEN'= $ENV:GITLAB_TOKEN } -Uri "https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml/raw?ref=main") | ConvertFrom-Yaml
 $template.version = $ENV:VERSION
 
 $body = @{
@@ -9,4 +9,4 @@ $body = @{
   content = "$($template | convertto-yaml)"
 } | ConvertTo-Json
 
-Invoke-RestMethod -Headers @{ 'PRIVATE-TOKEN'= $ENV:CI_JOB_TOKEN } -ContentType "application/json" -Method Put -body $body -Uri "https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml"
+Invoke-RestMethod -Headers @{ 'PRIVATE-TOKEN'= $ENV:GITLAB_TOKEN } -ContentType "application/json" -Method Put -body $body -Uri "https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml"
