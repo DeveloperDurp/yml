b9bec8b70f8f930d17de53d0633c776409d5384d
DeveloperDurpBot <DeveloperDurp@durp.info>
2024-03-31 14:31:08 +0000

    ci: render
    Rendered by https://gitlab.com/developerdurp/yml/-/pipelines/1234695239

---
+++ scripts/gitlab/update-chart.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#%%MULTILINE_YAML_START
+pwsh -c "Install-Module -Name powershell-yaml -Confirm:\$false -Force
+
+\$template = (Invoke-RestMethod -Headers @{ 'PRIVATE-TOKEN'= \$ENV:CI_JOB_TOKEN } `
+                               -Uri \"https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml/raw?ref=main\") | ConvertFrom-Yaml
+\$template.version = \$ENV:VERSION
+
+\$body = @{
+  branch = \"main\"
+  commit_message = \"Update Chart\"
+  content = \"\$(\$template | convertto-yaml)\"
+} | ConvertTo-Json
+
+Invoke-RestMethod -Headers @{ 'PRIVATE-TOKEN'= \$ENV:CI_JOB_TOKEN } `
+                  -ContentType \"application/json\" `
+                  -Method Put `
+                  -body \$body `
+                  -Uri \"https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml\""
