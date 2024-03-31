529d38db9ffc77b1ec3f3bf2b9c03a274f78c2dc
DeveloperDurp <DeveloperDurp@durp.info>
2024-03-31 10:02:00 -0500

    update

---
+++ scripts/golang-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#%%MULTILINE_YAML_START
+#Build golang project
+export GOPATH=/go
+export PATH=$PATH:$GOPATH/bin
+
+mkdir output
+
+go install 
+
+# Check if go.mod contains "swag"
+if grep -q "swag" go.mod; then
+    echo "Found 'swag' in go.mod. Running swag init..."
+    go install github.com/swaggo/swag/cmd/swag@v1.8.12 
+    swag init
+fi
+
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./output/main .
