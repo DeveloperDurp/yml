efa01db1ff87ee5c1fd0ccd185cdd81ada043b6c
DeveloperDurp <DeveloperDurp@durp.info>
2024-05-05 10:59:17 -0500

    update

--- scripts/scanner/grype.sh
+++ scripts/scanner/grype.sh
@@ -5,5 +5,5 @@
 for i in syft/*.sbom.json;
 do filename=${i%*.sbom.json};
   filename=${filename##/};
-  grype syft/$i -o json --file syft/$filename.cve.json;
+  grype $i -o json --file $filename.cve.json;
 done
