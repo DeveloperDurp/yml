ba38649a9f0de27a5b27a9b6b353d339188403e0
DeveloperDurp <DeveloperDurp@durp.info>
2024-05-05 08:52:27 -0500

    update

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -2,7 +2,7 @@
 #%%MULTILINE_YAML_START
 #Build Docker Container
 mkdir $CI_PROJECT_DIR/packages
-containername=$(echo "$PROJECT_NAME" | awk '{print tolower($0)}')
+containername=$(echo "$CI_PROJECT_NAME" | awk '{print tolower($0)}')
 
 docker build -t $containername .
 docker save -o $CI_PROJECT_DIR/packages/$containername.$VERSION.tar.gz $containername
