945ac257a15ac7be4cab41d690e34038f6c0df3a
DeveloperDurp <DeveloperDurp@durp.info>
2024-05-06 06:15:47 -0500

    update

--- scripts/scanner/syft-docker.sh
+++ scripts/scanner/syft-docker.sh
@@ -3,8 +3,8 @@
 #Syft scan for docker
 
 for i in packages/*.tar.gz;
-do filename=${i%.*.tar.gz};
+do filename=${i%.*.*.*.tar.gz};
   filename="$(basename -- "$filename")"
   syft $i -o cyclonedx-json=syft/$filename.docker.sbom.json;
 done
-
+${version%.*.*.*.tar.gz}
