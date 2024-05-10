dbd12cf6ab86aa1bd339e0b1a25a5c45fb33aaaf
DeveloperDurp <DeveloperDurp@durp.info>
2024-05-10 07:49:41 -0500

    update

--- scripts/gitlab/create-environment.sh
+++ scripts/gitlab/create-environment.sh
@@ -3,19 +3,10 @@
 #test deployment
 
 echo "
-Test_Job:
-  stage: deploy
-  script:
-    - echo "Test job"
-  environment:
-    name: $ENVIRONMENT
-
 $VERSION:
   stage: deploy
   needs:
     - job: Test_Job 
   script:
     - echo "Deploy to $ENVIRONMENT server"
-  environment:
-    name: $ENVIRONMENT
 " > generated-config.yml
