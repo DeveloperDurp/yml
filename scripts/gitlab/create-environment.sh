0657acd0c835ed13616587f54828a867947c0c22
DeveloperDurp <DeveloperDurp@durp.info>
2024-05-28 21:58:24 -0500

    update

--- scripts/gitlab/create-environment.sh
+++ scripts/gitlab/create-environment.sh
@@ -12,14 +12,14 @@ $VERSION:
 echo '
     - |
       pwsh -c "Install-Module -Name powershell-yaml -Confirm:\$false -Force
-      \$template = (Invoke-RestMethod -Headers @{ \"PRIVATE-TOKEN\"= \$ENV:GITLAB_TOKEN } -Uri \"https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml/raw?ref=dev\") | ConvertFrom-Yaml
+      \$template = (Invoke-RestMethod -Headers @{ \"PRIVATE-TOKEN\"= \$ENV:GITLAB_TOKEN } -Uri \"\$ENV:CI_API_V4_URL/projects/\$ENV:GITLAB_PROJECT_ID/repository/files/\$ENV:GITLAB_CHART_PATH/raw?ref=\$ENV:ENVIRONMENT\") | ConvertFrom-Yaml
       \$template.version = \$ENV:VERSION
       \$body = @{
-        branch = \"dev\"
+        branch = \"\$ENV:ENVIRONMENT\"
         commit_message = \"Update Chart\"
         content = \"\$(\$template | convertto-yaml)\"
       } | ConvertTo-Json
-      Invoke-RestMethod -Headers @{ \"PRIVATE-TOKEN\"= \$ENV:GITLAB_TOKEN } -ContentType \"application/json\" -Method Put -body \$body -Uri \"https://gitlab.com/api/v4/projects/45028985/repository/files/durpapi%2FChart.yaml\""
+      Invoke-RestMethod -Headers @{ \"PRIVATE-TOKEN\"= \$ENV:GITLAB_TOKEN } -ContentType \"application/json\" -Method Put -body \$body -Uri \"\$ENV:CI_API_V4_URL/projects/\$ENV:GITLAB_PROJECT_ID/repository/files/\$ENV:GITLAB_CHART_PATH\""
   needs:
     - pipeline: $PARENT_PIPELINE_ID
       job: version
