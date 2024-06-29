719018e0d6ac9207c49abe4b19f3d6bd07dcbdce
DeveloperDurp <DeveloperDurp@durp.info>
2024-06-29 09:00:38 -0500

    update

--- scripts/terraform/plan.sh
+++ scripts/terraform/plan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #%%MULTILINE_YAML_START
 #Terraform plan    
-apk add --update curl jq
+apk add --update curl jq 
 alias convert_report="jq -r '([.resource_changes[].change.actions?]|flatten)|{\"create\":(map(select(.==\"create\"))|length),\"update\":(map(select(.==\"update\"))|length),\"delete\":(map(select(.==\"delete\"))|length)}'"
 tofu plan -out=$PLAN $ARGUMENTS
-tofu show --json $PLAN | convert_report > $JSON_PLAN_FILE
+tofu show --json $PLAN | jq -r '([.resource_changes[].change.actions?]|flatten)|{"create":(map(select(.=="create"))|length),"update":(map(select(.=="update"))|length),"delete":(map(select(.=="delete"))|length)}' > $JSON_PLAN_FILE
